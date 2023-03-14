9e3d1bd21f198669ca8f58feeeedc12a7f6ee991
Hannah Tomio <tomio@mit.edu>
2023-03-14 09:47:03 -0400

    Include PyDSP modeling in link budget, revert for Python 3.10

--- OLBtools/__init__.py
+++ OLBtools/__init__.py
@@ -19,9 +19,6 @@ import scipy.optimize as scop
 import scipy.integrate as scin
 import scipy.interpolate as scit
 
-if not hasattr(inspect, 'getargspec'):
-    inspect.getargspec = inspect.getfullargspec
-
 mp.mp.dps = 300
 
 #----------------------------------------------------------
@@ -56,7 +53,7 @@ def extend_integ_axis(arrays,integ_var):
         ar = ar[..., np.newaxis]
     arl = np.broadcast_arrays(integ_var,h)
     return arl[1:]
-
+    
 mpin    = np.frompyfunc(mp.mpf,1,1)
 gamma   = np.frompyfunc(mp.gamma,1,1)
 besselk = np.frompyfunc(mp.besselk,2,1)
@@ -67,11 +64,10 @@ mpout   = np.frompyfunc(float,1,1)
 
 def beam_radius(W_0,G_Theta,G_Lambda):
     return W_0/np.sqrt(G_Theta**2 + G_Lambda**2)
-
+    
 def apply_min(x):
     return x #(x>min_log)*x + (x<=min_log)*min_log
-
-#TODO: just use dataclasses...
+    
 def initializer(func):
     """
     Automatically assigns the parameters.
@@ -86,7 +82,7 @@ def initializer(func):
     >>> p.cmd, p.reachable, p.user
     ('halt', True, 'root')
     """
-    names, varargs, varkw, defaults, kwonlyargs, kwonlydefaults, annotations = inspect.getargspec(func)
+    names, varargs, keywords, defaults = inspect.getargspec(func)
 
     @wraps(func)
     def wrapper(self, *args, **kargs):
@@ -113,24 +109,24 @@ def slant_range(h_0,H,zenith,Re):
     h_0 = h_0 + Re
     H = H + Re
     return np.sqrt( (h_0*np.cos(zenith))**2 + H**2 - h_0**2 ) - h_0*np.cos(zenith)
-
+    
 def earth_centered_intertial_to_latitude_longitude(latitude,longitude,thetaE,x,y,z):
     '''Peter Grenfell'''
 
     ctE = np.cos(thetaE);    stE = np.sin(thetaE)
     cph = np.cos(latitude);  sph = np.sin(latitude)
     cla = np.cos(longitude); sla = np.sin(longitude)
-
+    
     tx =  cla*cph*x + sla*cph*y + sph*z
     ty =     -sla*x +     cla*y
     tz = -cla*sph*x - sla*sph*y + cph*z
-
+    
     x =  ctE*tx + stE*ty
     y = -stE*tx + ctE*ty
     z = tz
-
+    
     return x,y,z
-
+    
 
 def pass_azimuth_elevation_and_time(latitude,longitude,altitude,inclination,min_zenith,max_zenith,npts,mu,Re,we):
     '''Peter Grenfell
@@ -138,123 +134,123 @@ def pass_azimuth_elevation_and_time(latitude,longitude,altitude,inclination,min_
     orbit_altitude
     min_zenith
     Re: distance from geotcenter to atmosphere 0 ref, in m'''
-
+    
     #Slant range at min zenith (max elevation)
     sr_min = slant_range(0,altitude,min_zenith,Re)
-
+    
     #Slant range at max zenith (min elevation)
     sr_max = slant_range(0,altitude,max_zenith,Re)
-
+    
     def law_of_cosines(a,b,c):
         #law of cosines, return angle, c is the far side
         return np.arccos( (a**2+b**2-c**2)/(2*a*b) )
-
+    
     #Top of pass in earth centered referential, at OGS lat/long
     x = altitude*np.sin(min_zenith)*np.cos(inclination)
     y = altitude*np.sin(min_zenith)*np.sin(inclination)
     h = Re + altitude*np.cos(min_zenith)
-
+    
     # Angle of OGS from orbit plan at geocenter.
     orbit_plan_angle_geocenter = law_of_cosines(Re,Re+altitude,sr_min)
-
+    
     # Angle of the horizon at geocenter from vertical (zentith = 0), in corbit plane.
     horizon_angle_in_plane_geocenter = law_of_cosines(Re,Re+altitude,sr_max)
-
-    # Angle of horizon our of orbit plane, using spherical pythagorean theorem.
+    
+    # Angle of horizon our of orbit plane, using spherical pythagorean theorem. 
     horizon_angle_geocenter = np.arccos( np.cos(horizon_angle_in_plane_geocenter) / np.cos(orbit_plan_angle_geocenter) )
-
+    
     # Orbit periode
     T_orbit = 2*np.pi*np.sqrt( (altitude+Re)**3/mu )
-
+    
     # time to the horizon from vertical (zentith = 0)
     t_horizon = horizon_angle_geocenter*T_orbit/(2*np.pi)
-
+    
     # Timescale
     t = np.linspace(-t_horizon,t_horizon,npts)
-
+    
     # Orbit phase
     sat_phase = 2*np.pi*t/T_orbit
-
+    
     #Top of pass in earth centered inertial
     #x,y,z = earth_centered_intertial_to_latitude_longitude(-latitude,-longitude,0,x=h,y=x,z=y)
-
+    
     # Circular equatorial orbit
     x_orbit = (Re + altitude)*np.cos(sat_phase)
     y_orbit = (Re + altitude)*np.sin(sat_phase)
     z_orbit = 0
-
+    
     # offset from OGS, rotation arround y axis
     coff = np.cos(orbit_plan_angle_geocenter)
     soff = np.sin(orbit_plan_angle_geocenter)
     x_off =  coff*x_orbit
     y_off =  y_orbit
     z_off =  soff*x_orbit
-
+    
     # Adding inclnation, rotation arround x axis
     cinc = np.cos(inclination)
     sinc = np.sin(inclination)
     x_inc = x_off
     y_inc =  cinc*y_off + sinc*z_off
     z_inc = -sinc*y_off + cinc*z_off
-
+   
     # Putting it on top off OGS at t0
     x, y, z  = earth_centered_intertial_to_latitude_longitude(latitude,longitude,0,x_inc,y_inc,z_inc)
-
+    
     # OGS coordinates
     x0,y0,z0 = earth_centered_intertial_to_latitude_longitude(latitude,longitude,we*t,Re,0,0)
     x0,y0,z0 = np.broadcast_arrays(x0,y0,z0)
     ogs = np.array([x0,y0,z0]).transpose()
-
+    
     if 0:
         import matplotlib.pyplot as plt
         from mpl_toolkits.mplot3d import Axes3D
-
+    
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
         ax.plot(x,y,z)
         ax.scatter(x0,y0,z0)
         ax.scatter(0,0,0)
-
-    # Sat vector from OGS
+    
+    # Sat vector from OGS 
     xs = x-x0
     ys = y-y0
     zs = z-z0
     ns = np.sqrt(xs**2 + ys**2 + zs**2)
-
+    
     # satelite unit vector
     sat = np.array([xs/ns,ys/ns,zs/ns]).transpose()
-
+    
     # Zentih angle at OGS
     zenith = np.arccos((x0*xs + y0*ys + z0*zs)/ns/Re)
-
+    
     # Sat unit vector in OGS horizontal plane
     sat_horizontal = np.cross(ogs,sat)/Re #90 deg away
-
+    
     # East vector at OGS
     east = np.cross([0,0,1],ogs)/Re
-
+    
     # North vector at OGS
     north = np.cross(ogs,east)/Re
-
+    
     def dot_2D(a,b):
         return a[:,0]*b[:,0] + a[:,1]*b[:,1] + a[:,2]*b[:,2]
-
+    
     cos_east  = dot_2D(sat_horizontal,east)
     cos_north = dot_2D(sat_horizontal,north)
-
+    
     # Azimuth angle at OGS
     azimuth = np.arctan2(cos_north,-cos_east)
-
+    
     #on top of lat/long = 0
     #+z
     #^
     #|
     #O->+y
     #+x
-
-
+    
+    
     return t,zenith,azimuth
-
+    
 #----------------------------------------------------------
 # Link budget functions
 #----------------------------------------------------------
@@ -283,7 +279,7 @@ def path_loss_gaussian(beam_radius, wavelength, distance, rx_diameter, pointing_
     # Calculate dB loss
     path_loss_dB = 10*np.log10(rx_power) # [dB]
     return path_loss_dB
-
+    
 def fwhm_to_radius(fwhm, wavelength):
     # From Matlab linkbudget, author O cierny
     '''Calculates the 1/e^2 Gaussian beam radius given the full width half
@@ -292,7 +288,7 @@ def fwhm_to_radius(fwhm, wavelength):
     beam_radius: 1/e^2 beam radius at origin [m]'''
     beam_radius = (wavelength * np.sqrt(2*np.log(2))) / (np.pi * fwhm)
     return beam_radius
-
+    
 #----------------------------------------------------------
 # LOWTRAN functions
 #----------------------------------------------------------
@@ -306,7 +302,7 @@ def LOWTRAN_transmittance(zenith,lambda_min,lambda_max,step=1e7/20,model=5,H=0,h
     lambda_max: longest wavelength cm^-1 ???
     step: wavelength step size
     '''
-
+    
     import lowtran
 
     c1 = {'model': model,
@@ -319,35 +315,35 @@ def LOWTRAN_transmittance(zenith,lambda_min,lambda_max,step=1e7/20,model=5,H=0,h
           }
 
     TR = lowtran.transmittance(c1)
-
+    
     return TR
-
+    
 def transmittance(zenith,wavelength,model,H):
-
+    
     wavelength = np.asarray(wavelength)
-
+    
     wl_step_large=1e7/20
     #wl_step_min=1e7/5
 
     lambda_low  = 1/(1/wavelength.min() + wl_step_large*1e-2)
     lambda_high = 1/(1/wavelength.max() - wl_step_large*1e-2)
-
+    
     #degrees(zenith)
     T = LOWTRAN_transmittance(zenith,lambda_low,lambda_high,wl_step_large,model,H)
-
+    
     x = T.wavelength_nm[::-1]*1e-9
     y = T['transmission'][0,::-1,:]
     w = wavelength
-
+    
     if w.shape: ws = w.shape[0]
     else: ws = 1
-
+    
     r = np.zeros((ws,y.shape[1]))
     for n in range(y.shape[1]):
        r[:,n] = np.interp(w, x, y[:,n])
-
+    
     return r
-
+    
 #def MODTRAN_transmittance(zenith
 
 class Quadcell:
@@ -360,36 +356,36 @@ class Quadcell:
         amplifier_noise: output refered noise of the amplifier, in v/rtHz
         bandwidth: bandwidth for the detection, in Hz
         '''
-
+        
         #Quadrants defined as A,B,C,D, in trigonometric order, in quadcell front view.
         # A: +x,+y, B:-x,+y, C:-x,-y, D:+x,-y
-
+        
         #Cumulative sum of the PSF over each quadrant, as a scipy interpolation function
         self.PSF_sum_2D_A = None
         self.PSF_sum_2D_B = None
         self.PSF_sum_2D_C = None
         self.PSF_sum_2D_D = None
-
+        
     def cumulated_gaussian_PSF_on_square_mask(W,x1,x2,y1,y2):
         #W: spot size for the PSF, such as PSF(r) = 2/(pi*W**2)*exp(-2*r**2/w**2)
         coef = np.sqrt(2)/W
         return 0.25*(scsp.erf(coef*x2)-scsp.erf(coef*x1))*(scsp.erf(coef*y2)-scsp.erf(coef*y1))
-
+        
     def cumulated_gaussian_PSF_on_quadrant_mask(W,x1,y1):
         #W: spot size for the PSF, such as PSF(r) = 2/(pi*W**2)*exp(-2*r**2/w**2)
         coef = np.sqrt(2)/W
         return 0.25*(1-scsp.erf(coef*x1))*(1-scsp.erf(coef*y1))
-
+        
     def set_PSF_gaussian(self,W):
         #W: spot size for the PSF, such as PSF(r) = 2/(pi*W**2)*exp(-2*r**2/w**2)
-
-        def culative_sum_function(x,y):
+        
+        def culative_sum_function(x,y):            
             x_shifted = self.gap/2-x
             y_shifted = self.gap/2-y
             return Quadcell.cumulated_gaussian_PSF_on_quadrant_mask(W,x_shifted,y_shifted)
-
+            
         self.set_all_quadrant_sum(culative_sum_function)
-
+        
     def set_PSF_1D(self,r_samp,v_samp,n_int2d=1000,deg=3):
         ''' Compute and set the cumulative sum of the PSF for each quadrant, using radial PSF samples
             The PSF values are linearly interpolated
@@ -405,27 +401,27 @@ class Quadcell:
         v2 = v_samp[1:]
         normalize = np.pi*np.sum(r2**2*v2 + (r1**2+r1*r2+r2**2)*(v1-v2)/3 - r1**2*v1)
         v_samp = v_samp/normalize
-
+        
         rf = r_samp[-1]
         x = np.linspace(-rf,rf,n_int2d)
         y = np.linspace(-rf,rf,n_int2d)
         xx, yy = np.meshgrid(x,y)
         r = np.sqrt(xx**2+yy**2)
         v = (r<=rf)*np.interp(r,r_samp,v_samp)
-
+        
         v_integ = np.cumsum(np.cumsum(v,axis=0),axis=1)*(2*rf/n_int2d)**2
 
         v_integ_lookup = scit.RectBivariateSpline(x-self.gap/2,y-self.gap/2,v_integ,kx=deg,ky=deg)
 
         self.set_all_quadrant_sum(v_integ_lookup)
-
+        
     def set_all_quadrant_sum(self,sum_function):
         # Set all quadrant if the PSF is axi-sytmetrical.
         self.PSF_sum_2D_A = lambda x,y,dx=0,dy=0:sum_function( x, y,dx,dy,grid=False)
         self.PSF_sum_2D_B = lambda x,y,dx=0,dy=0:sum_function(-x, y,dx,dy,grid=False)
         self.PSF_sum_2D_C = lambda x,y,dx=0,dy=0:sum_function(-x,-y,dx,dy,grid=False)
         self.PSF_sum_2D_D = lambda x,y,dx=0,dy=0:sum_function( x,-y,dx,dy,grid=False)
-
+    
     def set_PSF_2D(self,x_samp,y_samp,v_samp,n_int2d=1000,deg=3):
         ''' Compute and set the cumulative sum of the PSF for each quadrant, 2D grid PSF samples
             The PSF values are linearly interpolated
@@ -436,36 +432,36 @@ class Quadcell:
         n_int2d resolution of the cumulative sum on the quadrant
         deg: interpolation degree for the cumulative sum on the quadrant
         '''
-
+        
         normalize = np.sum(v_samp)*(x_samp[1]-x_samp[0])*(y_samp[1]-y_samp[0])
         v_samp = v_samp/normalize
-
+        
         # PSF linear interpolation
         x = np.linspace(x_samp[0],x_samp[-1],n_int2d)#[:, np.newaxis]
         y = np.linspace(y_samp[0],y_samp[-1],n_int2d)#[np.newaxis, :]
         xx, yy = np.meshgrid(x,y)
         v_samp_lookup = scit.RectBivariateSpline(x_samp,y_samp,v_samp,kx=1,ky=1)
-
+        
         dx_dy_integ_coef = (1/n_int2d)**2
         dx_dy_integ_coef *= (x_samp[-1]-x_samp[0])*(y_samp[-1]-y_samp[0])
-
+        
         #integration over each quadrant, and sum interpolation function, shifted by gap value
         v = v_samp_lookup( xx, yy, grid=False)
         v_integ = np.cumsum(np.cumsum(v,axis=0),axis=1)*dx_dy_integ_coef
         self.PSF_sum_2D_A = lambda vx,vy,dx=0,dy=0: (scit.RectBivariateSpline(x+self.gap/2,y+self.gap/2,v_integ,kx=deg,ky=deg))( vx, vy,dx,dy,grid=False)
-
+        
         v = v_samp_lookup(-xx, yy, grid=False)
         v_integ = np.cumsum(np.cumsum(v,axis=0),axis=1)*dx_dy_integ_coef
         self.PSF_sum_2D_B = lambda vx,vy,dx=0,dy=0: (scit.RectBivariateSpline(x+self.gap/2,y+self.gap/2,v_integ,kx=deg,ky=deg))(-vx, vy,dx,dy,grid=False)
-
+        
         v = v_samp_lookup(-xx,-yy, grid=False)
         v_integ = np.cumsum(np.cumsum(v,axis=0),axis=1)*dx_dy_integ_coef
         self.PSF_sum_2D_C = lambda vx,vy,dx=0,dy=0: (scit.RectBivariateSpline(x+self.gap/2,y+self.gap/2,v_integ,kx=deg,ky=deg))(-vx,-vy,dx,dy,grid=False)
-
+        
         v = v_samp_lookup( xx,-yy, grid=False)
         v_integ = np.cumsum(np.cumsum(v,axis=0),axis=1)*dx_dy_integ_coef
         self.PSF_sum_2D_D = lambda vx,vy,dx=0,dy=0: (scit.RectBivariateSpline(x+self.gap/2,y+self.gap/2,v_integ,kx=deg,ky=deg))( vx,-vy,dx,dy,grid=False)
-
+        
     def eval_quadrants(self,x_spot,y_spot,dx=0,dy=0):
         #Normalized quadrant amplitude for a give spot postition
         A = self.PSF_sum_2D_A(x_spot,y_spot,dx,dy)
@@ -473,7 +469,7 @@ class Quadcell:
         C = self.PSF_sum_2D_C(x_spot,y_spot,dx,dy)
         D = self.PSF_sum_2D_D(x_spot,y_spot,dx,dy)
         return A,B,C,D
-
+        
     def response_from_quadrants(self,quadrants):
         #give the slope of the quadcell response for given quadrant values
         A,B,C,D = quadrants
@@ -481,62 +477,62 @@ class Quadcell:
         x_resp = ((A+D)-(B+C))/quad_sum
         y_resp = ((A+B)-(C+D))/quad_sum
         return x_resp, y_resp
-
+    
     def response(self,x_spot,y_spot):
         #give the quadcell response for a give spot position
         return self.response_from_quadrants(self.eval_quadrants(x_spot,y_spot))
-
+        
     def slope_from_quadrant(self,quadrants,quadrants_da):
         #give the slope of the quadcell response for given quadrant values and derivative
         A,B,C,D = quadrants
         A_da,B_da,C_da,D_da = quadrants_da
         quad_sum = A+B+C+D
         quad_sum_da = A_da+B_da+C_da+D_da
-
+        
         x_resp_num = (A+D)-(B+C)
         x_resp_num_da = (A_da+D_da)-(B_da+C_da)
         y_resp_num = (A+B)-(C+D)
         y_resp_num_da = (A_da+B_da)-(C_da+D_da)
-
+        
         x_resp_da = (quad_sum*x_resp_num_da - x_resp_num*quad_sum_da)/quad_sum**2
         y_resp_da = (quad_sum*y_resp_num_da - y_resp_num*quad_sum_da)/quad_sum**2
-
+        
         return x_resp_da,y_resp_da
-
+        
     def slope(self,x_spot,y_spot):
         #give the slope of the quadcell response for a given spot position
         quad    = self.eval_quadrants(x_spot,y_spot,dx=0,dy=0)
         quad_dx = self.eval_quadrants(x_spot,y_spot,dx=1,dy=0)
         quad_dy = self.eval_quadrants(x_spot,y_spot,dx=0,dy=1)
-
+        
         x_resp_dx,y_resp_dx = self.slope_from_quadrant(quad,quad_dx)
         x_resp_dy,y_resp_dy = self.slope_from_quadrant(quad,quad_dy)
-
+        
         return x_resp_dx,x_resp_dy,y_resp_dx,y_resp_dy
-
+        
     def angular_slope(self,x_spot,y_spot,focal_lenght,magnification=1):
         x_resp_dx,x_resp_dy,y_resp_dx,y_resp_dy = self.slope(x_spot,y_spot)
-
+        
         x_angle = magnification*np.arctan2(x_spot,focal_lenght)
         y_angle = magnification*np.arctan2(x_spot,focal_lenght)
-
+        
         x_resp_dx = x_resp_dx/x_spot*x_angle
         x_resp_dy = x_resp_dy/x_spot*x_angle
         y_resp_dx = y_resp_dx/y_spot*y_angle
         y_resp_dy = y_resp_dy/y_spot*y_angle
-
+        
         return x_resp_dx,x_resp_dy,y_resp_dx,y_resp_dy
-
+        
     def SNR(self,x_spot,y_spot,optical_power):
         ''' Return the quacel signal SNR
         x_spot, y_spot: postion of the spot on the quadcell in m or PSF postition units
         optical_power: total spot power, W
-
+        
         '''
-
+        
         A,B,C,D = self.eval_quadrants(x_spot,y_spot,dx=0,dy=0)
         all_quadrants = np.stack((A,B,C,D))
-
+        
         all_power = all_quadrants*optical_power
         print('power',all_power[0][750][750])
         all_current = all_power*self.responsivity
@@ -547,18 +543,18 @@ class Quadcell:
         all_shot_noise = self.transimpedance*np.sqrt(2*qe*all_current*self.bandwidth)
         all_amp_noise = self.amplifier_noise*np.sqrt(self.bandwidth)
         all_noise = np.sqrt(all_shot_noise**2+all_amp_noise**2)
-
+        
         signal = sum([all_voltage[i,:,:] for i in range(4)])
         noise = np.sqrt(sum([all_noise[i,:,:]**2 for i in range(4)]))
         SNR = signal / noise
-
+        
         return SNR
-
+        
     def NEA(self,x_spot,y_spot,optical_power,focal_lenght,magnification=1):
         slopes = self.angular_slope(x_spot,y_spot,focal_lenght,magnification)
         slope = np.sqrt(sum([s**2 for s in slopes]))
         SNR = self.SNR(x_spot,y_spot,optical_power)
-
+        
         NEA = 1/SNR/slope
         return NEA
 
@@ -566,20 +562,20 @@ class Photodiode:
     @initializer #automaticaly add agurment to each instances
     def __init__(self,gain=1,responsivity=0.5,bandwidth=1e6,excess_noise_factor=1,dark_current=0):
         pass
-
+    
     def signal(self,optical_power):
         return self.gain*self.responsivity*optical_power
-
+    
     def noise(self,optical_power):
         return np.sqrt(2*qe*self.excess_noise_factor*(self.signal(optical_power)+self.dark_current)*self.bandwidth)
-
+        
     def SNR(self,optical_power):
         #Defined as (I/In)**2
         return (self.signal(optical_power)/self.noise(optical_power))**2
-
+        
 def BER_OOK(SNR):
     return 0.5*scsp.erfc(np.sqrt(SNR)/(2*np.sqrt(2)))
-
+    
 def BER_OOK_integrated(SNR,PDF):
     BER = BER_OOK(SNR)
     BER_out_of_pdf = 0.5*( 1- np.sum(PDF, axis = 0) )
@@ -599,7 +595,7 @@ def gaussian_PSF_on_square_mask(W,x1,x2,y1,y2):
     #W: spot size for the PSF, such as PSF(r) = 2/(pi*W**2)*exp(-2*r**2/w**2)
     coef = np.sqrt(2)/W
     return 0.25*(scsp.erf(coef*x2)-scsp.erf(coef*x1))*(scsp.erf(coef*y2)-scsp.erf(coef*y1))
-
+    
 def sampled_linear_interpolation_PSF_corner(r_samp,v_samp,n_int2d=1000,deg=3):
     r1 = r_samp[:-1]
     r2 = r_samp[1:]
@@ -614,26 +610,26 @@ def sampled_linear_interpolation_PSF_corner(r_samp,v_samp,n_int2d=1000,deg=3):
     xx, yy = np.meshgrid(x,y)
     r = np.sqrt(xx**2+yy**2)
     v = (r<=rf)*np.interp(r,r_samp,v_samp)
-
+    
     v_integ = np.cumsum(np.cumsum(v,axis=0),axis=1)*(2*rf/n_int2d)**2
 
     v_integ_lookup = scit.RectBivariateSpline(x,y,v_integ,kx=deg,ky=deg)
 
     return v_integ_lookup
-
+    
 def sampled_2D_interpolation_PSF_corner(x_samp,y_samp,v_samp,n_int2d=1000,deg=3):
     normalize = np.sum(v_samp)*(x_samp[1]-x_samp[0])*(y_samp[1]-y_samp[0])
     v_samp = v_samp/normalize
     center_x = np.sum(v_samp*x_samp)/np.sum(v_samp)
     center_y = np.sum(v_samp*y_samp)/np.sum(v_samp)
     #print(v_samp.shape)
-
+    
     x = np.linspace(x_samp[0],x_samp[-1],n_int2d)
     y = np.linspace(y_samp[0],y_samp[-1],n_int2d)
     xx, yy = np.meshgrid(x,y)
     v_samp_lookup = scit.RectBivariateSpline(x_samp,y_samp,v_samp,kx=1,ky=1)
     v = v_samp_lookup(x,y)
-
+    
     v_integ = np.cumsum(np.cumsum(v,axis=0),axis=1)*((x_samp[-1]-x_samp[0])/n_int2d)*((y_samp[-1]-y_samp[0])/n_int2d)
 
     v_integ_lookup = scit.RectBivariateSpline(x,y,v_integ,kx=deg,ky=deg)
--- coherent_test.py
+++ coherent_test.py
@@ -5,8 +5,35 @@ Created on Mon Mar 13 17:23:35 2023
 
 @author: htomio
 """
+import sys
 import OLBtools as olb
 import numpy as np
+import math
+
+from tabulate import tabulate
+from scipy.signal import welch
+import matplotlib.pyplot as plt
+
+## Add path to PyDSP
+sys.path.append(r"C:\Users\Hannah\Documents\workspace")
+
+from PyDSP_core.TX.Tx_generateBits import Tx_generateBits, nextpow2
+from PyDSP_core.TX.setSignalParams import setSignalParams
+from PyDSP_core.TX.pulseShaper import pulseShaper
+from PyDSP_core.TX.QAM_config import QAM_config
+from PyDSP_core.TX.Tx_QAM import Tx_QAM
+from PyDSP_core.TX.laserCW import laserCW
+
+from PyDSP_core.RX.setSNR import setSNR
+
+from PyDSP_core.DSP.symDemapper import symDemapper
+from PyDSP_core.DSP.rxDecision import BER_eval, EVM_eval, MI_eval
+from PyDSP_core.DSP.LPF_apply import LPF_apply
+
+
+#----------------------------------------------------------
+# LINK
+#----------------------------------------------------------
 
 # Transmit
 wavelength = 1550e-9 # m
@@ -36,3 +63,160 @@ print('Link gain: %f dB' % all_losses)
 
 rx_power = tx_power*10**(all_losses/10)
 print('Power received: %f nW' % (rx_power*1e9))
+
+SNR_dB = 10*np.log10(rx_power)
+print('Power received: %f dB' % (SNR_dB))
+
+#----------------------------------------------------------
+# COHERENT
+#----------------------------------------------------------
+
+### Transmitter Parameters
+
+#Signal Parameters:
+SIG = {
+    'M': 16,            #QAM constellation size
+    'symRate': 60e9,    #total symbol-rate of the signal
+    'modulation': 'QAM',#modulation type [QAM/PAM]
+    'rollOff': 0.1,     #roll-off factor
+    'nPol': 1,          #number of polarizations
+    'nSyms': pow(2, 17) #total number of simulated symbols
+}
+nSpS = 2                #number of samples per symbol
+laserLW = 1e3           #laser linewidth [Hz]
+TX = {'SIG': setSignalParams('symRate', SIG['symRate'], 'M', SIG['M'], 'nPol', SIG['nPol'],
+                             'nBpS', math.log2(SIG['M']), 'nSyms', SIG['nSyms'],
+                             'roll-off', SIG['rollOff'], 'modulation', SIG['modulation'])
+}
+
+#Modulation Parameters:
+TX['QAM'] = QAM_config(TX['SIG'])
+#Bit Parameters:
+TX['BIT'] = {'source': 'randi',
+             'seed': 1
+}
+#Pulse Shaping Filter Parameters:
+TX['PS'] = {'type': 'RRC',
+            'rollOff': TX['SIG']['rollOff'],
+            'nTaps': 128
+}
+#DAC Parameters:
+TX['DAC'] = {'RESAMP': {'sampRate':nSpS * TX['SIG']['symRate']}
+}
+#Laser Parameters:
+TX['LASER'] = {
+    'linewidth': laserLW,
+}
+
+### DSP Parameters
+
+#Matched-Filter Parameters:
+DSP = {'MF':{'type': 'RRC',
+             'rollOff': TX['SIG']['rollOff']}
+}
+#Carrier-Phase Estimation:
+DSP['CPE'] = {
+    'method': 'VV',
+    'decision': 'QPSKpartition',
+    'QAM_classes': 'all',
+    'nTaps': 50
+}
+#Demapper:
+DSP['DEMAPPER'] = {
+    'normMethod': 'MMSE'
+}
+
+### Generate TX Bits
+TX['BIT']['txBits'] = Tx_generateBits(SIG['nSyms'], TX['QAM']['M'], TX['QAM']['nPol'], TX['BIT'])
+
+### Generate Transmitted Symbols
+S = {}
+S['tx'], TX['BIT']['txSyms'] = Tx_QAM(TX['QAM'], TX['BIT']['txBits'])
+
+# Plot Transmitted Constellation Symbols:
+plt.scatter(S['tx'].real, S['tx'].imag, s=20)
+plt.title('IQ Constellation of the Transmitted Signal')
+plt.show()
+
+### Pulse Shaping
+S['txSC'], TX['PS'] = pulseShaper(S['tx'], nSpS, TX['PS'])
+
+#Plot Pulse Shaping Taps:
+plt.plot(TX['PS']['W'])
+plt.title('Pulse Shaping Taps')
+plt.show()
+
+# Plot Signal Spectrum:
+# No matlab: pwelch(S.txSC(1,:),1e4,[],[],TX.DAC.RESAMP.sampRate,'centered')
+#                  (x, window, noverlap, nfft, Fs, freqrange)
+#
+# window - If window is an integer, the signal is divided into segments of length window. The modified periodograms are computed using a Hamming window of length window.
+# noverlap - If you do not specify noverlap, or specify noverlap as empty, the default number of overlapped samples is 50% of the window length.
+# nfft - The default nfft is the greater of 256 or the next power of 2 greater than the length of the segments.
+
+wind = np.hamming(pow(10,4))
+nov = np.floor(pow(10,4)/2)
+nff = max(256, pow(2,nextpow2(pow(10,4))))
+
+f,Pxx = welch(S['txSC'][0],window = np.hamming(int(1e4)),noverlap=nov,nfft=nff,fs=TX['DAC']['RESAMP']['sampRate'],return_onesided=False ,detrend= False)
+plt.plot(f,10*np.log10(Pxx))
+plt.grid()
+plt.show()
+
+### Apply Laser
+
+Fs_SC = nSpS * SIG['symRate']
+nSamples = S['tx'].shape[1] * nSpS
+Scw, TX['LASER'] = laserCW(TX['LASER'],Fs_SC,nSamples)
+if SIG['nPol'] == 2:
+    Scw = Scw/np.sqrt(2)
+for n in range(0, SIG['nPol']):
+    S['txSC'][n,:] = Scw*S['txSC'][n,:]
+
+# Plot Laser Phase Noise
+plt.plot(TX['LASER']['phaseNoise'],linewidth=0.5)
+#plt.title('Transmitted Laser Phase Noise')
+plt.xlabel('Samples')
+plt.ylabel('Phase Noise')
+plt.show()
+
+### Set SNR
+SNR_dB = float(SNR_dB)
+S['rx'] = setSNR(S['txSC'], SNR_dB, TX['DAC']['RESAMP']['sampRate'], SIG['symRate'])[0]
+
+### Apply Matched Filter
+S['rx'] = LPF_apply(S['rx'],DSP['MF'],TX['DAC']['RESAMP']['sampRate'],SIG['symRate'])[0]
+
+### Downsample to 1 sample/symbol
+S['rx_1sps'] = S['rx'][:,::nSpS]
+
+### Apply Symbol Demapper
+C = TX['QAM']['IQmap']
+DSP['DEMAPPER'],S['tx'] = symDemapper(S['rx_1sps'],S['tx'],C,DSP['DEMAPPER'])
+
+### Performance Analysis
+BER = np.zeros(SIG['nPol'])
+errPos = []
+EVM = np.zeros(SIG['nPol'])
+MI = np.zeros(SIG['nPol'])
+for n in range(0, SIG['nPol']):
+    BER[n], errPos_aux = BER_eval(DSP['DEMAPPER']['txBits'][n],DSP['DEMAPPER']['rxBits'][n])
+    errPos.append(errPos_aux)
+    EVM[n] = EVM_eval(S['rx_1sps'][n],S['tx'][n])
+    MI[n] = MI_eval(S['rx_1sps'][n],S['tx'][n],DSP['DEMAPPER']['C'][:,n],DSP['DEMAPPER']['N0'][n])
+    
+### Plot Constellation after Demapping
+for n in range(0, SIG['nPol']):
+    plt.scatter(S['rx_1sps'][n].real, S['rx_1sps'][n].imag, s=0.1)
+    #plt.scatter(DSP['DEMAPPER']['C'][:,n].real, DSP['DEMAPPER']['C'][:,n].imag,s=50, marker='o', edgecolor='k', facecolor=(0,1,0,0))
+    #plt.title('pol-'+str(n+1))
+    plt.xlabel('Inphase')
+    plt.ylabel('Quadrature')
+    plt.show()
+    
+### Print Results
+parameters = ['BER', 'EVM', 'MI']
+columns = np.asarray([BER,EVM,MI])
+header = [f"Polarization-{i}" for i in range(1,SIG['nPol'] + 1)]
+
+print(tabulate(columns,headers=header,tablefmt= 'fancy_grid',showindex=parameters,numalign='right'))
